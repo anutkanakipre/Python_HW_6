74393f79fd554924a9cd6d637ec87df6e7dbdfcc
Anna <anutkanakipre@gmail.com>
2023-01-20 12:57:44 +0500

    lambda.py

---
+++ enumerate.py
@@ -0,0 +1,28 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+# *Пример:*
+
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+### Без enumerate ###
+
+# num = int(input("Ведите число: "))
+
+# dvoichnoe = "{0:b}".format(num)
+# dvoichnoe = bin(num)
+# print (dvoichnoe)
+
+### C enumerate ###
+
+
+num = int(input("Ведите число: "))
+def dvoichnoe(x):
+    res = "{0:b}".format(x)
+    res = bin(x)
+    return res
+NewList = [num]
+result = map(dvoichnoe, NewList)
+result = enumerate(list(result))
+print(list(result))
